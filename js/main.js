21bdaf1035dad6e9eec064ef81665501bba486cf
Jonathan Geiger <tantangeiger@gmail.com>
2021-08-06 22:17:54 +0300

    popup added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
-'use strict'
+'use strict';
 
-function toggleMenu(){
-    document.body.classList.toggle('menu-open');
+function toggleMenu() {
+  document.body.classList.toggle('menu-open');
+}
+
+function toggleModal() {
+  if (document.body.classList.contains('modal-open')) {
+    document.querySelector('.popup-modal').style.display = 'none';
+    document.body.classList.toggle('modal-open');
+    return;
+  }
+  document.querySelector('.popup-modal').style.display = 'block';
+  document.body.classList.toggle('modal-open');
 }
